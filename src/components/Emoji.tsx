cd92aa5b2e85747fe52ba29165dab80ba5995d6a
pritvikraj <pritvik7911@gmail.com>
2026-05-07 16:56:22 +0530

    changed emojis to stars

--- src/components/Emoji.tsx
+++ src/components/Emoji.tsx
@@ -1,23 +1,41 @@
-import bullsEye from "../assets/bulls-eye.webp";
-import thumbsUp from "../assets/thumbs-up.webp";
-import meh from "../assets/meh.webp";
-import type { ImageProps } from "@chakra-ui/react";
-import { Image } from "@chakra-ui/react";
+// import bullsEye from "../assets/bulls-eye.webp";
+// import thumbsUp from "../assets/thumbs-up.webp";
+// import meh from "../assets/meh.webp";
+// import type { ImageProps } from "@chakra-ui/react";
+// import { Image } from "@chakra-ui/react";
 
 interface Props {
   rating: number;
 }
 
+// const Emoji = ({ rating }: Props) => {
+//   if (rating < 3) return null;
+
+//   const emojiMap: { [key: number]: ImageProps } = {
+//     3: { src: meh, alt: "meh", boxSize: "25px" },
+//     4: { src: thumbsUp, alt: "recommended", boxSize: "25px" },
+//     5: { src: bullsEye, alt: "exceptional", boxSize: "35px" },
+//   };
+
+//   return <Image {...emojiMap[rating]} boxSize="25px" marginTop={1} />;
+// };
+
+// export default Emoji;
+
+//stars
+
+import { HStack } from "@chakra-ui/react";
+import { FaStar } from "react-icons/fa";
+
 const Emoji = ({ rating }: Props) => {
-  if (rating < 3) return null;
-
-  const emojiMap: { [key: number]: ImageProps } = {
-    3: { src: meh, alt: "meh", boxSize: "25px" },
-    4: { src: thumbsUp, alt: "recommended", boxSize: "25px" },
-    5: { src: bullsEye, alt: "exceptional", boxSize: "35px" },
-  };
-
-  return <Image {...emojiMap[rating]} boxSize="25px" marginTop={1} />;
+  if (rating < 1) return null;
+  return (
+    <HStack>
+      {Array.from({ length: rating }).map((_, i) => (
+        <FaStar key={i} color="#cdcd4d" size={15}></FaStar>
+      ))}
+    </HStack>
+  );
 };
 
 export default Emoji;
--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -18,11 +18,12 @@ const GameCard = ({ game }: Props) => {
           <PlatformIconList
             platforms={game.parent_platforms?.map((p) => p.platform)}
           />
-          <CriticScore score={game.metacritic}></CriticScore>
+          <CriticScore score={game.metacritic} />
         </HStack>
-        <Heading fontSize={"2xl"}>
-          {game.name} <Emoji rating={game.rating_top} />
-        </Heading>
+        <HStack justifyContent={"space-between"} alignItems={"flex-end"}>
+          <Heading fontSize={"2xl"}>{game.name}</Heading>
+          <Emoji rating={game.rating_top} />
+        </HStack>
       </CardBody>
     </Card>
   );
